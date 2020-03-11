678be9e254fd28a8b4a8d6e9759a0edf57cd6d82
Denys <chornijj15@gmail.com>
2020-03-11 16:08:22 +0200

    Make my own tabs using only HTML and Pure JavaScript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 window.addEventListener('DOMContentLoaded', function(){
+
     'use strict';
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
+        
     function hideTabContent(a){
         for( let i = a; i<tabContent.length; i++){
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
-
     hideTabContent(1);
 
     function showTabContent(b){
@@ -28,6 +30,8 @@ window.addEventListener('DOMContentLoaded', function(){
                     hideTabContent(0);
                     showTabContent(i);
                     break;
+
+
                 }
             }
         }
