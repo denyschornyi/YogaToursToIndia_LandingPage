cf06796f3ef939acfe7ed39561c5d14cc5aa3515
Denys <chornijj14@gmail.com>
2020-03-14 23:26:18 +0200

    Learned basics ES6 of JS. Create element by constructor by class and this and added that block to page

--- js/script.js
+++ js/script.js
@@ -113,13 +113,32 @@ window.addEventListener('DOMContentLoaded', function(){
         });
     });
     
+    
+        
+   class Options{
+       constructor(height, width, bg, fontSize, textAlign){
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+       }
+
+        createDiv() {
+            let div = document.createElement('div');
+            div.textContent = 'lorem lorem lorem babah';
+            div.style.cssText = `height: ${this.height}px;
+                                width: ${this.width}px;
+                                background: ${this.bg};
+                                font-size: ${this.fontSize}px;
+                                text-align: ${this.textAlign};`;
+            document.body.appendChild(div);
+        }
 
         
-   
-
-
-    
-
+   }
+   let block = new Options(200,200,"red",40,"center");
+   block.createDiv();
     
 });
 
