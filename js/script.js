7d8b679b466761eca22c368a70b352a751ff2730
Denys <chornijj14@gmail.com>
2020-04-02 22:49:52 +0300

    Add modificator place and this.value add

--- js/script.js
+++ js/script.js
@@ -269,6 +269,35 @@ let form          = document.querySelector('.main-form'),
 
         totalValue.textContent = 0;
 
+        persons.addEventListener('input', function(){
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+            if(restDays.value == ''){
+                totalValue.textContent = 0;
+            }else{
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('input', function(){
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+            if(persons.value == ''){
+                totalValue.textContent = 0;
+            }else{
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function(){
+            if(restDays.value == '' || persons.value ==''){
+                totalValue.innerHTML = 0;
+            }else{
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
+
 
 
 });
